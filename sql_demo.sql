6b741a133c0ccf33eb12ef23d284f89909fc070e
fullstack060721 <87032460+fullstack060721@users.noreply.github.com>
2021-08-06 11:45:33 +0300

    Update sql_demo.sql

--- sql_demo.sql
+++ sql_demo.sql
@@ -8,3 +8,18 @@ language plpgsql AS
 
 
 select * from hello_world();
+
+create or replace FUNCTION sp_sum(a double precision, b double precision) RETURNS double precision
+language plpgsql AS
+    $$
+        DECLARE
+            sum double precision := 0;
+        BEGIN
+            sum := a + b;
+            RETURN sum;
+        END;
+    $$;
+
+select * from sp_sum(4.5, 12.888);
+
+
